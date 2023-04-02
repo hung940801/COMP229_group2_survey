a13176aea2ce937b0ecd151692879a8cde6f1037
Jack Lam <jack15cm@gmail.com>
2023-04-02 21:51:50 +0800

    Add comments

--- server/models/score.js
+++ server/models/score.js
@@ -2,6 +2,12 @@ let mongoose = require('mongoose');
 let scoreModel = mongoose.Schema({
     question_id    : String,
     score      : Number,
+    // 1- Strong Disagree
+    // 2- Disagree
+   //  3-Neutral
+   //  4- Agree
+  //   5 Strongly Agree
+
     survey_id           : String
 },
 {
